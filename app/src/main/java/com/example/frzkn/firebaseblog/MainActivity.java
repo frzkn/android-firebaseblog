d68b6f74b9c21c2e1e19e6ee87b3c9df1e917db1
frzkn <farazrk001@gmail.com>
2018-03-29 17:20:50 +0530

    Add RegisterAct and Toolbar

--- app/src/main/java/com/example/frzkn/firebaseblog/MainActivity.java
+++ app/src/main/java/com/example/frzkn/firebaseblog/MainActivity.java
@@ -3,20 +3,27 @@ package com.example.frzkn.firebaseblog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Intent intent;
+    private Intent intent;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intent = new Intent(MainActivity.this, LoginActivity.class);
-
-
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getActionBar() != null) {
+            getActionBar().setTitle("Home");
+        }
 
     }
 
@@ -25,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser != null) {
-            Toast.makeText(this,"User logged in ",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "User logged in ", Toast.LENGTH_SHORT).show();
 
-        } else if (currentUser == null){
-            Toast.makeText(this,"User not logged in ",Toast.LENGTH_SHORT).show();
+        } else if (currentUser == null) {
+            Toast.makeText(this, "User not logged in ", Toast.LENGTH_SHORT).show();
             startActivity(intent);
             //User cannot return to this screen again if they press the back button
             //Intent will be cleared
@@ -36,4 +43,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 }
