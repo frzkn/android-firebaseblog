f61330e4bd7e9dcf1f17903d8314c31290b1f9c4
frzkn <farazrk001@gmail.com>
2018-03-29 17:28:45 +0530

    Add Logout code

--- app/src/main/java/com/example/frzkn/firebaseblog/MainActivity.java
+++ app/src/main/java/com/example/frzkn/firebaseblog/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,12 +13,15 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
+    private FirebaseAuth mAuth;
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initialize firebase
+        mAuth = FirebaseAuth.getInstance();
         intent = new Intent(MainActivity.this, LoginActivity.class);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,17 +39,40 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "User logged in ", Toast.LENGTH_SHORT).show();
 
         } else if (currentUser == null) {
-            Toast.makeText(this, "User not logged in ", Toast.LENGTH_SHORT).show();
-            startActivity(intent);
-            //User cannot return to this screen again if they press the back button
-            //Intent will be cleared
-            finish();
+            intentToLogin();
+
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                firebaseLogout();
+            default:
+                return false;
+        }
+    }
+
+    private void firebaseLogout() {
+        mAuth.signOut();
+        intentToLogin();
+
+    }
+
+
+    private void intentToLogin() {
+        Toast.makeText(this, "User not logged in ", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+        //User cannot return to this screen again if they press the back button
+        //Intent will be cleared
+        finish();
     }
 }
